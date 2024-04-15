0a1ce347bd0b64123287024d0dc013fb5b487625
nileshunique <nilesh.ignou15@gmail.com>
2024-04-15 20:32:05 +0530

    production setup created

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -35,3 +35,7 @@ const Message = (props) => {
 }
 
 export default Message
+
+
+// user image link
+//  https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png
